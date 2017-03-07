173d1900a2df4cf176870087c427ba2f9374dfe6
igor <igor@microlab.ctrlnet>
2017-03-07 21:19:49 +0300

    preview version

--- remexec-web.php
+++ remexec-web.php
@@ -1,4 +1,17 @@
 <?php
-	var_dump($_POST);
-?>
+///	var_dump($_POST);
 
+	$tmpfname = tempnam("/tmp", "source").".cpp";
+	$srcfile = fopen($tmpfname, "w");
+	fwrite($srcfile, $_POST['code']);
+	fclose($srcfile);
+
+	$command = "./remexec cpp ".escapeshellcmd($tmpfname);
+	$handle = popen($command.' 2>&1', 'r');
+	while ($read = fgets($handle)){
+		echo htmlspecialchars ($read)."<br>";
+	}
+	pclose($handle);
+
+	unlink($srcfile);
+?>
